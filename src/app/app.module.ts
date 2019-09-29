5767271660159025b13a496290f811be77c4e097
Christopher Hoffman <CSH.Hoffman@gmail.com>
2019-09-29 14:24:19 -0400

    Switched common components over to a Shared Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './core/http/auth-interceptor.service';
 import { CallbackComponent } from './shared/callback/callback.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SharedModule } from './shared/shared.module';
 import { UserProfileComponent } from './shared/user-profile/user-profile.component';
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    CallbackComponent,
-    UserProfileComponent,
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    HttpClientModule,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
     {
       multi: true,

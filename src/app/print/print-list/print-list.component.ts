2af35521330dc1318cbb17d4be8f1c92c79d6a69
Christopher Hoffman <CSH.Hoffman@gmail.com>
2020-04-19 18:15:02 -0400

    Removed the unused "Include Inactive" checkbox that was accidently copy/pasted.

--- src/app/print/print-list/print-list.component.ts
+++ src/app/print/print-list/print-list.component.ts
@@ -28,7 +28,6 @@ export class PrintListComponent implements OnInit {
     'status',
   ];
 
-  public includeInactive = false;
   public searchText = '';
 
   public filterByStatus: PrintStatus | null = -1;

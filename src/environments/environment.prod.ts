efac4017dd20ba2b0a91752c7a9e4e7028304678
Christopher Hoffman <CSH.Hoffman@gmail.com>
2020-04-19 22:05:07 -0400

    Setup Production values.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true,
+  printLogApiUrl: 'https://3d-print-log-api.azurewebsites.net',
+  authentication: {
+    domain: '3dprintlog.auth0.com',
+    client_id: '3JOtliMnZIQJDZ5auHia2GZEydfXW9xB',
+    audience: 'https://3dprintlog.com/api',
+  },
 };

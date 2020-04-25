cdb944b4ea75ea2ad97730450a0118d099b8071d
Christopher Hoffman <CSH.Hoffman@gmail.com>
2020-04-25 13:49:32 -0400

    Added the PrintsByStatus pie graph using the new donut component.

--- src/app/analytics/panels/graph-panel/graph-panel.component.ts
+++ src/app/analytics/panels/graph-panel/graph-panel.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface GraphData {}
 
 @Component({
   selector: 'app-graph-panel',
   templateUrl: './graph-panel.component.html',
-  styleUrls: ['./graph-panel.component.scss']
+  styleUrls: ['./graph-panel.component.scss'],
 })
 export class GraphPanelComponent implements OnInit {
+  @Input() title: string;
+  @Input() data;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/analytics/prints-by-status/prints-by-status.component.ts
+++ src/app/analytics/prints-by-status/prints-by-status.component.ts
@@ -1,5 +1,23 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { DonutChartComponent } from '../panels/donut-chart/donut-chart.component';
 import { PrintStatistic } from '../services/print-statistics.service';
+
+import { MatSlideToggleChange } from '@angular/material';
+import { groupBy } from 'lodash';
+import { PrintStatus } from 'src/app/print/services/print.service';
+
+export interface PrintStatusState {
+  status: string;
+  stateDisplayValue: string;
+  count: number;
+}
 
 @Component({
   selector: 'app-prints-by-status',
@@ -9,17 +27,78 @@ import { PrintStatistic } from '../services/print-statistics.service';
 export class PrintsByStatusComponent implements OnChanges {
   @Input() prints: PrintStatistic[] = [];
 
-  public printCount: number;
+  @ViewChild('ordersByStatusChart', { static: true })
+  chart: DonutChartComponent;
+
+  orderStates: PrintStatusState[] = [...this.getEmptyState()];
+
+  chartData: number[] = [0, 0, 0, 0, 0];
+
+  displayedColumns = ['legend', 'orderStatus', 'total'];
+
+  colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#8c8c8c8a', '#d62728'];
+
+  showData = false;
 
   constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.prints) {
-      this.calculatePrintCount();
+      this.updatePrintStatuses();
     }
   }
 
-  calculatePrintCount() {
-    this.printCount = this.prints.length;
+  updatePrintStatuses() {
+    this.orderStates = [...this.getEmptyState()];
+    const groups = groupBy(this.prints, 'status');
+
+    for (const group in groups) {
+      // check also if property is not inherited from prototype
+      if (groups.hasOwnProperty(group)) {
+        this.orderStates.find(
+          state => state.status === PrintStatus[group]
+        ).count = groups[group].length;
+      }
+    }
+
+    this.chartData = [];
+    this.orderStates.forEach(state => {
+      this.chartData.push(state.count);
+    });
+  }
+
+  getEmptyState() {
+    const emptyState: PrintStatusState[] = [
+      {
+        status: PrintStatus[PrintStatus.Pending],
+        stateDisplayValue: PrintStatus[PrintStatus.Pending],
+        count: 0,
+      },
+      {
+        status: PrintStatus[PrintStatus.Printing],
+        stateDisplayValue: PrintStatus[PrintStatus.Printing],
+        count: 0,
+      },
+      {
+        status: PrintStatus[PrintStatus.Success],
+        stateDisplayValue: PrintStatus[PrintStatus.Success],
+        count: 0,
+      },
+      {
+        status: PrintStatus[PrintStatus.Cancelled],
+        stateDisplayValue: PrintStatus[PrintStatus.Cancelled],
+        count: 0,
+      },
+      {
+        status: PrintStatus[PrintStatus.Failed],
+        stateDisplayValue: PrintStatus[PrintStatus.Failed],
+        count: 0,
+      },
+    ];
+    return emptyState;
+  }
+
+  toggleData(event: MatSlideToggleChange) {
+    this.showData = event.checked;
   }
 }

716b86f7bc809f61b3c3f70c8906efd275fd54ff
Christopher Hoffman <CSH.Hoffman@gmail.com>
2020-04-24 23:05:06 -0400

    Fixed an issue where a publicly used component was marked as private.

--- src/app/documentation/docs/docs-getting-started/docs-getting-started.component.ts
+++ src/app/documentation/docs/docs-getting-started/docs-getting-started.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./docs-getting-started.component.scss'],
 })
 export class DocsGettingStartedComponent implements OnInit {
-  constructor(public authService: AuthService, private router: Router) {}
+  constructor(public authService: AuthService, public router: Router) {}
 
   ngOnInit() {}
 }

d1cb6fa7965a21e663bb66d9cd389af38de40745
Christopher Hoffman <CSH.Hoffman@gmail.com>
2020-04-25 19:30:01 -0400

    Added a Prompt to redirect to the Add Printer screen if there are no active 3D printers for the user.

---
+++ src/app/print/services/printer-redirect-prompt.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PrinterService } from 'src/app/core/services/printer.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PrinterRedirectPromptService {
+  /**
+   * We only want to show the prompt once per login.
+   */
+  private hasShownPromptBefore = false;
+
+  constructor(private printerService: PrinterService) {}
+
+  shouldShowAddPrinterPrompt(): Observable<boolean> {
+    if (this.hasShownPromptBefore) {
+      return of(false);
+    }
+
+    return this.printerService
+      .getCurrentUserPrinterSummaries(1, 100, undefined, false)
+      .pipe(
+        map(results => {
+          const hasPrinters = results.paging.totalCount > 0;
+          const shouldShowPrompt = !hasPrinters;
+
+          return shouldShowPrompt;
+        })
+      );
+  }
+}

a11105d511946e48e712274ec8ac4259f305b87e
Christopher Hoffman <CSH.Hoffman@gmail.com>
2020-05-07 20:56:14 -0400

    Moved the image resizing helper from the Print Service into it's own reusable service.

--- src/app/print/services/print.service.ts
+++ src/app/print/services/print.service.ts
@@ -15,6 +15,10 @@ import { environment } from 'src/environments/environment';
 
 import * as moment from 'moment';
 
+import {
+  ImageResizerService,
+  IResizeImageOptions,
+} from 'src/app/core/services/image-resizer.service';
 import { PagedList } from 'src/app/core/types/paging';
 import { SortDirection } from 'src/app/core/types/sort-request';
 
@@ -55,6 +59,7 @@ export interface PrintSummary {
   status: PrintStatus;
 
   defaultPrintImageId: number;
+  createdByUserId: number;
 }
 
 export interface PrintDetailDTO {
@@ -71,9 +76,9 @@ export interface PrintDetailDTO {
   notes: string;
   url: string;
   status: PrintStatus;
-
   viewStatus: PrintViewStatus;
   images?: PrintImage[];
+  createdByUserId: number;
 }
 
 export interface PutPrintDetailDTO {
@@ -111,6 +116,7 @@ export interface PrintDetail {
   viewStatus: PrintViewStatus;
 
   images?: PrintImage[];
+  createdByUserId: number;
 }
 
 /**
@@ -132,11 +138,6 @@ export interface AddPrintDTO {
   viewStatus: PrintViewStatus;
 }
 
-interface IResizeImageOptions {
-  maxSize: number;
-  file: File;
-}
-
 @Injectable()
 export class PrintService {
   private readonly baseApi = environment.printLogApiUrl;
@@ -144,7 +145,10 @@ export class PrintService {
   private readonly IMAGE_QUALITY = 0.9;
   private readonly IMAGE_MAX_SIZE_PX = 1280;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private imageResizer: ImageResizerService
+  ) {}
 
   getPrintSummaries(
     pageNumber: number = 1,
@@ -198,6 +202,7 @@ export class PrintService {
             url: newPrint.url,
             viewStatus: newPrint.viewStatus,
             images: newPrint.images || [],
+            createdByUserId: newPrint.createdByUserId,
           };
           return print;
         })
@@ -278,9 +283,10 @@ export class PrintService {
     const settings: IResizeImageOptions = {
       file,
       maxSize: this.IMAGE_MAX_SIZE_PX,
+      imageQuality: this.IMAGE_QUALITY,
     };
 
-    return from(this.resizeImage(settings)).pipe(
+    return from(this.imageResizer.resizeImage(settings)).pipe(
       switchMap((reducedImage) => {
         const formData: FormData = new FormData();
         formData.append('image', reducedImage, file.name);
@@ -326,60 +332,4 @@ export class PrintService {
 
     return this.http.delete(url);
   }
-
-  resizeImage = (settings: IResizeImageOptions): Promise<Blob> => {
-    const file = settings.file;
-    const maxSize = settings.maxSize;
-    const reader = new FileReader();
-    const image = new Image();
-    const canvas = document.createElement('canvas');
-    const dataURItoBlob = (dataURI: string) => {
-      const bytes =
-        dataURI.split(',')[0].indexOf('base64') >= 0
-          ? atob(dataURI.split(',')[1])
-          : unescape(dataURI.split(',')[1]);
-      const mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
-      const max = bytes.length;
-      const ia = new Uint8Array(max);
-      for (let i = 0; i < max; i++) {
-        ia[i] = bytes.charCodeAt(i);
-      }
-      return new Blob([ia], { type: mime });
-    };
-    const resize = () => {
-      let width = image.width;
-      let height = image.height;
-
-      if (width > height) {
-        if (width > maxSize) {
-          height *= maxSize / width;
-          width = maxSize;
-        }
-      } else {
-        if (height > maxSize) {
-          width *= maxSize / height;
-          height = maxSize;
-        }
-      }
-
-      canvas.width = width;
-      canvas.height = height;
-      canvas.getContext('2d').drawImage(image, 0, 0, width, height);
-      const dataUrl = canvas.toDataURL('image/jpeg', this.IMAGE_QUALITY);
-      return dataURItoBlob(dataUrl);
-    };
-
-    return new Promise((ok, no) => {
-      if (!file.type.match(/image.*/)) {
-        no(new Error('Not an image'));
-        return;
-      }
-
-      reader.onload = (readerEvent: any) => {
-        image.onload = () => ok(resize());
-        image.src = readerEvent.target.result;
-      };
-      reader.readAsDataURL(file);
-    });
-  };
 }

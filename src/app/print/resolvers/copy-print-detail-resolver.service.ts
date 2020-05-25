4223929a6bb26a086202884b11eac90e3629b612
Christopher Hoffman <CSH.Hoffman@gmail.com>
2020-05-25 14:01:53 -0400

    The changes to the PrintDetail Resolves to include the user broke the copying of prints. This fixes the copy ability.

--- src/app/print/resolvers/copy-print-detail-resolver.service.ts
+++ src/app/print/resolvers/copy-print-detail-resolver.service.ts
@@ -4,6 +4,7 @@ import {
   Resolve,
   RouterStateSnapshot,
 } from '@angular/router';
+import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {
   PrintDetail,
@@ -11,9 +12,11 @@ import {
   PrintService,
   PrintStatus,
 } from '../services/print.service';
+import { PrintDetailWithUser } from './print-detail-resolver.service';
 
 @Injectable()
-export class CopyPrintDetailResolverService implements Resolve<PrintDetail> {
+export class CopyPrintDetailResolverService
+  implements Resolve<PrintDetailWithUser> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const printId = +route.paramMap.get('id');
 
@@ -21,12 +24,22 @@ export class CopyPrintDetailResolverService implements Resolve<PrintDetail> {
       return this.printService.getPrintDetail(printId).pipe(
         map((print) => {
           const cleanedPrint: PrintDetail = { ...print, id: null, images: [] };
-          return cleanedPrint;
+
+          const newDetails: PrintDetailWithUser = {
+            print: cleanedPrint,
+            user: null,
+          };
+
+          return newDetails;
         })
       );
     }
 
-    return null;
+    const emptyDetail: PrintDetailWithUser = {
+      print: null,
+      user: null,
+    };
+    return emptyDetail;
   }
 
   constructor(private printService: PrintService) {}

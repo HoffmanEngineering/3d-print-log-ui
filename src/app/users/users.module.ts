51dcbb32a5df2e9ba89d345334da0059a98fd995
Christopher Hoffman <CSH.Hoffman@gmail.com>
2020-06-11 21:58:04 -0400

    Added the User's StatsComponent to the UserProfile.

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 import { AdsenseModule } from 'ng2-adsense';
 import { SharedModule } from '../shared/shared.module';
 import { UserDetailResolverService } from './resolvers/user-detail-resolver.service';
+import { StatsComponent } from './stats/stats.component';
 import { UserPrintsComponent } from './user-prints/user-prints.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
 @NgModule({
-  declarations: [UsersComponent, UserProfileComponent, UserPrintsComponent],
+  declarations: [
+    UsersComponent,
+    UserProfileComponent,
+    UserPrintsComponent,
+    StatsComponent,
+  ],
   imports: [SharedModule, UsersRoutingModule, AdsenseModule],
   providers: [UserDetailResolverService],
 })

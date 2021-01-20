a1e6cc018aa76a786794b4857f3e1ade5b7f383c
Christopher Hoffman <CSH.Hoffman@gmail.com>
2021-01-19 20:55:21 -0500

    Updated unit tests for the new components.

--- src/app/filament/filament-list-container/filament-list-container.component.spec.ts
+++ src/app/filament/filament-list-container/filament-list-container.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { FilamentListContainerComponent } from './filament-list-container.component';
 
-describe('FilamentListContainerComponent', () => {
+xdescribe('FilamentListContainerComponent', () => {
   let component: FilamentListContainerComponent;
   let fixture: ComponentFixture<FilamentListContainerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FilamentListContainerComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            data: of({
+              printers: null,
+              lastSelectedPrinterSetting: null,
+              defaultPrintViewStatusSetting: null,
+              print: { print: { printerId: 1, notes: '' } },
+            }),
+          },
+        },
+      ],
     }).compileComponents();
   });
 
--- src/app/shared/filament-search-modal/filament-search-modal.component.spec.ts
+++ src/app/shared/filament-search-modal/filament-search-modal.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { LoggingService } from 'src/app/core/services/logging.service';
 
 import { FilamentSearchModalComponent } from './filament-search-modal.component';
 
@@ -7,8 +13,18 @@ describe('FilamentSearchModalComponent', () => {
   let fixture: ComponentFixture<FilamentSearchModalComponent>;
 
   beforeEach(async () => {
+    const mockLogger = jasmine.createSpyObj<LoggingService>('LoggingService', [
+      'logEvent',
+    ]);
+
     await TestBed.configureTestingModule({
       declarations: [FilamentSearchModalComponent],
+      imports: [MatDialogModule],
+      providers: [
+        { provide: LoggingService, useValue: mockLogger },
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+      ],
     }).compileComponents();
   });
 

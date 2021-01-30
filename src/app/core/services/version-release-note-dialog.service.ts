309649a6ea9738d0f261a994b5c1b252acc821ec
Christopher Hoffman <CSH.Hoffman@gmail.com>
2021-01-30 09:11:23 -0500

    Added filament usage on the homepage, fixed an issue with the settings component not setting the title correctly.

--- src/app/core/services/version-release-note-dialog.service.ts
+++ src/app/core/services/version-release-note-dialog.service.ts
@@ -27,6 +27,9 @@ export class VersionReleaseNoteDialogService {
   private readonly LOCAL_STORAGE_KEY = 'LastLoggedInVersion';
 
   private releaseNotes: ReleaseNoteHistory = {
+    '1.7.3': {
+      redirect: '1.7.2',
+    },
     '1.7.2': {
       redirect: '1.7.1',
     },

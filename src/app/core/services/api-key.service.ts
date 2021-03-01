ccee8e0379a4ae0dee4990a6f451279d6bd74276
Christopher Hoffman <CSH.Hoffman@gmail.com>
2021-02-28 20:03:52 -0500

    Fixed linting errors.

--- src/app/core/services/api-key.service.ts
+++ src/app/core/services/api-key.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import * as moment from 'moment';
 
 export interface UserApiKeySummary {
   /** GUID */

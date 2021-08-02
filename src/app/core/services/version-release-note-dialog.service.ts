8a8545e88f22f724085d1e93d4d8aaf87080f7aa
Christopher Hoffman <CSH.Hoffman@gmail.com>
2021-08-01 20:18:36 -0400

    Fixed an issue with image centering in the Home and Print Image components.

--- src/app/core/services/version-release-note-dialog.service.ts
+++ src/app/core/services/version-release-note-dialog.service.ts
@@ -27,6 +27,9 @@ export class VersionReleaseNoteDialogService {
   private readonly LOCAL_STORAGE_KEY = 'LastLoggedInVersion';
 
   private releaseNotes: ReleaseNoteHistory = {
+    '1.12.8': {
+      redirect: '1.12.7',
+    },
     '1.12.7': {
       redirect: '1.12.6',
     },

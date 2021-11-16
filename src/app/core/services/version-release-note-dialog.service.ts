a1df908d4631e3114a4dba0b229c06ad75c1a97e
Christopher Hoffman <CSH.Hoffman@gmail.com>
2021-11-15 19:34:44 -0500

    Added release notes for 1.14.1

--- src/app/core/services/version-release-note-dialog.service.ts
+++ src/app/core/services/version-release-note-dialog.service.ts
@@ -27,6 +27,9 @@ export class VersionReleaseNoteDialogService {
   private readonly LOCAL_STORAGE_KEY = 'LastLoggedInVersion';
 
   private releaseNotes: ReleaseNoteHistory = {
+    '1.14.1': {
+      redirect: '1.14.0',
+    },
     '1.14.0': {
       title: '1.14.0 - Gcode Parsing for all Slicers',
       body: `<p>

22562d1d825be21b503469cd52d2af5791cf48c1
Christopher Hoffman <CSH.Hoffman@gmail.com>
2022-04-30 10:47:23 -0400

    Add 1.16.1 release notes

--- src/app/core/services/version-release-note-dialog.service.ts
+++ src/app/core/services/version-release-note-dialog.service.ts
@@ -27,6 +27,9 @@ export class VersionReleaseNoteDialogService {
   private readonly LOCAL_STORAGE_KEY = 'LastLoggedInVersion';
 
   private releaseNotes: ReleaseNoteHistory = {
+    '1.16.1': {
+      redirect: '1.16.0',
+    },
     '1.16.0': {
       title: '1.16.0 - Customizable Print List',
       body: `<p>

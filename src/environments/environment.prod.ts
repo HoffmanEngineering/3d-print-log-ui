05ca148998dfec2406d2d64a5c36e349b0dc20ce
Christopher Hoffman <CSH.Hoffman@gmail.com>
2022-05-22 14:27:55 -0400

    Update Google Analytics to GA4 (from Universal Analytics)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
     client_id: '3JOtliMnZIQJDZ5auHia2GZEydfXW9xB',
     audience: 'https://3dprintlog.com/api',
   },
-  googleAnalyticsId: 'UA-65004387-6',
+  googleAnalyticsId: 'G-4TEMNSY6QX',
   appInsights: {
     instrumentationKey: '3c9f6914-555b-4442-8724-0d5b706d115a',
   },

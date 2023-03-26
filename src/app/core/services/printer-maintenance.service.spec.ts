8ca7ebf673b36ffc444813e6dd5b5c041e94a432
Christopher Hoffman <CSH.Hoffman@gmail.com>
2023-03-26 14:52:38 -0400

    Fix service unit test

--- src/app/core/services/printer-maintenance.service.spec.ts
+++ src/app/core/services/printer-maintenance.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { PrinterMaintenanceService } from './printer-maintenance.service';
@@ -6,7 +7,7 @@ describe('PrinterMaintenanceService', () => {
   let service: PrinterMaintenanceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(PrinterMaintenanceService);
   });
 

ee296a64da8c4d45e3df4430b08dbb74d72474ee
Christopher Hoffman <CSH.Hoffman@gmail.com>
2026-02-21 20:50:06 -0500

    fix: remove OnPush from FeedComponent to restore feed rendering

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,11 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
   standalone: false,
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeedComponent {
   constructor() {}

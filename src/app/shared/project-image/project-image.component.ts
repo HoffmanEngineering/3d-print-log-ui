c7a70f8dff74e1c96db0786bb9947f2becefa7d5
Christopher Hoffman <CSH.Hoffman@gmail.com>
2026-04-19 18:47:58 -0400

    fix: use effect and takeUntilDestroyed in ProjectImageComponent

--- src/app/shared/project-image/project-image.component.ts
+++ src/app/shared/project-image/project-image.component.ts
@@ -1,11 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  OnInit,
+  DestroyRef,
+  effect,
   inject,
   input,
   signal,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatIconModule } from '@angular/material/icon';
 import { ProjectService } from 'src/app/core/services/project.service';
 
@@ -16,20 +18,26 @@ import { ProjectService } from 'src/app/core/services/project.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [MatIconModule],
 })
-export class ProjectImageComponent implements OnInit {
+export class ProjectImageComponent {
   private readonly projectService = inject(ProjectService);
+  private readonly destroyRef = inject(DestroyRef);
 
   projectId = input.required<string>();
   imageId = input<number | undefined>(undefined);
 
   imageData = signal('');
 
-  ngOnInit(): void {
-    const id = this.imageId();
-    if (id) {
-      this.projectService
-        .getProjectImage(this.projectId(), id)
-        .subscribe((data) => this.imageData.set(data));
-    }
+  constructor() {
+    effect(() => {
+      const id = this.imageId();
+      if (id) {
+        this.projectService
+          .getProjectImage(this.projectId(), id)
+          .pipe(takeUntilDestroyed(this.destroyRef))
+          .subscribe((data) => this.imageData.set(data));
+      } else {
+        this.imageData.set('');
+      }
+    });
   }
 }

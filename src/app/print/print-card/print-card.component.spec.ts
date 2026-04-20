921e5d0ec947efaafe4a929cccd27e61f3575c62
Christopher Hoffman <CSH.Hoffman@gmail.com>
2026-04-20 18:12:07 -0400

    fix: use app-print-image in PrintCardComponent template

--- src/app/print/print-card/print-card.component.spec.ts
+++ src/app/print/print-card/print-card.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialog } from '@angular/material/dialog';
 import { By } from '@angular/platform-browser';
@@ -33,6 +35,8 @@ describe('PrintCardComponent', () => {
       imports: [PrintCardComponent, NoopAnimationsModule],
       providers: [
         provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting(),
         {
           provide: MatDialog,
           useValue: jasmine.createSpyObj('MatDialog', ['open']),

ad28d66f324d2b820c3d5b90503f655191f48840
Christopher Hoffman <CSH.Hoffman@gmail.com>
2026-04-27 15:14:05 -0400

    feat: add electricity cost setting resolvers

---
+++ src/app/core/resolvers/default-electricity-wattage-setting-resolver.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  UserSettingService,
+  UserSettingType,
+} from 'src/app/core/services/user-setting.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DefaultElectricityWattageSettingResolverService {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.userSettingService.getCurrentUsersSettingByType(
+      UserSettingType.Electricity_DefaultWattageW
+    );
+  }
+  constructor(private readonly userSettingService: UserSettingService) {}
+}

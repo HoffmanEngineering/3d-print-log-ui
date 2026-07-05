ffc681cf18c2973e067a68b6e447736e1a6ec627
Christopher Hoffman <CSH.Hoffman@gmail.com>
2026-07-05 19:20:04 -0400

    fix: register FlexLayoutServerModule so prerender renders flex layout cleanly

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { FlexLayoutServerModule } from '@ngbracket/ngx-layout/server';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { serverRoutes } from './app.routes.server';
 
 @NgModule({
-  imports: [AppModule],
+  imports: [AppModule, FlexLayoutServerModule],
   providers: [provideServerRendering(withRoutes(serverRoutes))],
   bootstrap: [AppComponent],
 })
